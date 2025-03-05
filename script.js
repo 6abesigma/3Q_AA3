b0a8c34ee630db5ccd2636ae6d2f256675e32e22
6abesigma <cgcdavis22@src.pshs.edu.ph>
2025-03-05 15:30:07 +0800

    script.js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-function messageVerifier() {
-    let messageToVerify = prompt('Enter a message to verify.')
+function messageVerify() {
+	let messageToVerify = prompt('Enter a message to verify.')
 
-    if ((messageToVerify.indexOf('FR') >= 0) && (messageToVerify.indexOf('AI') >= 0 )) {
-        document.getElementById('result').innerHTML = `The message ${messageToVerify} is very legitimate! Info is very safe :p`;
-    }
-    
-        else if (messageToVerify.indexOf('AI') >= 0 || messageToVerify.indexOf('aI') >= 0 || messageToVerify.indexOf('Ai') >= 0) {
-        document.getElementById('result').innerHTML = `The message ${messageToVerify} is tampered!!!!!!!!! D:<<< DO NOT LISTEN TO THIS INFO.`;
-    }
-
-        else if (messageToVerify.indexOf('FR') >= 0) {
-        document.getElementById('result').innerHTML = `The message ${messageToVerify} is legitimate! ^__^ `;
-    }
-
-        else {
-        document.getElementById('result').innerHTML = `The message ${messageToVerify} yet to be encoded... Try again next time!`;
-    }
+	if ((messageToVerify.indexOf('FR') >= 0) && (messageToVerify.indexOf('AI') >= 0)) {
+		document.getElementById('result').innerHTML = `The message ${messageToVerify} is legit frfr!`;
+	} else if (messageToVerify.indexOf('AI') >= 0 || messageToVerify.indexOf('aI') >= 0 || messageToVerify.indexOf('Ai') >= 0) {
+		document.getElementById('result').innerHTML = `The message ${messageToVerify} is so not real grrrr`;
+	} else if (messageToVerify.indexOf('FR') >= 0) {
+		document.getElementById('result').innerHTML = `The message ${messageToVerify} is legitimate on skibidi`;
+	} else {
+		document.getElementById('result').innerHTML = `The message ${messageToVerify} not encoded yet yo try again yo!`;
+	}
 }
